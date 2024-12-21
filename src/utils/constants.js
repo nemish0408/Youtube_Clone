72880a1e62b8ee2fff86f1f0d8f387b6ed893fb7
Nemish Bhojani <133897690+nemish0408@users.noreply.github.com>
2024-12-21 11:13:03 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-const GOOGLE_API_KEY = "AIzaSyBuzxLA42LphcGD9heSbItVbYg7ZdYMY94"
+const GOOGLE_API_KEY = "AIzaSyAFTBObt3QD_IgjszqpLndXCejwxi8zuqg"
 
 export const YOUTUBE_VIDEO_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY
