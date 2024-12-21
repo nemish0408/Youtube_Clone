9de9a230ae05161ca84dd9ad6a8228e64a41d25f
nemish0408 <bhojaninemish1234@gmail.com>
2024-12-21 12:42:10 +0530

    api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
+
 const GOOGLE_API_KEY = "AIzaSyD7AS6YMvFdPomjG4pbto9P79_L1oQwRK8";
-=======
-const GOOGLE_API_KEY = "AIzaSyAFTBObt3QD_IgjszqpLndXCejwxi8zuqg"
->>>>>>> 72880a1e62b8ee2fff86f1f0d8f387b6ed893fb7
 
 export const YOUTUBE_VIDEO_URL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
