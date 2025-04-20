3d1bd6b83c6df2c3e7baac2458b296848bed07c8
Nemish Bhojani <bhojaninemish1234@gmail.com>
2025-04-20 22:50:51 +0530

    env

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 module.exports = {
-  darkMode: 'class', // or 'media'
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust to match your file structure
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [
     require('tailwind-scrollbar'),
-    
   ],
-  
 };
